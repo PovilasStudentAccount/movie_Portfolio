8d937164d423ddf58c43e6ae35e73e13824e468b
PovilasStudentAccount <povilas.glebus@utb.salem.se>
2022-10-03 13:36:53 +0200

    Moved root to main css. changed link colors to small text color and headings to main text color. Made a functioning slick carousel slider with some css issues for the moment.

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,35 @@
 
- $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slick-slider'
-  });
+ 
   $('.slick-slider').slick({
-    slidesToShow: 3,
+    dots: true,
+    infinite: true,
     slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    centerMode: true,
-    focusOnSelect: true
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
