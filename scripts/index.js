7949499ff5cff1fe35b33f8110565ad8e0dddbe7
PovilasStudentAccount <povilas.glebus@utb.salem.se>
2022-10-11 12:34:35 +0200

    Slick slider more resolution responsive. Fixed production page wrapper Co-Authored by Marcus

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,14 @@
   $('.slick-slider').slick({
     dots: true,
     infinite: true,
+    slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -17,15 +18,15 @@
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       }
       // You can unslick at a given breakpoint now by adding:
