4548f0ba060dde1f1462853d03321e95f4d3ede6
BuildTools <unconfigured@null.spigotmc.org>
2023-02-24 16:46:55 +0100

    minified all CSS and JS files

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,46 @@
-
-// Modal code
-
-
-document.addEventListener('click', function (e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-    //array of videos
-    var myPlayer = [videojs("vid1"),
-                    videojs("vid2"),
-                    videojs("vid3"),
-                    videojs("vid4")];
-
-    //video js options loop for array access
-    for (let i = 0; i < myPlayer.length; i++){
-
-        //make all videos responsive to resolution
-        myPlayer[i].aspectRatio('16:9');
-        myPlayer[i].fluid(true);
+document.addEventListener(
+  "click",
+  function (e) {
+    var target = (e = e || window.event).target || e.srcElement,
+    //array for videos
+      myPlayer = [
+        videojs("vid1"),
+        videojs("vid2"),
+        videojs("vid3"),
+        videojs("vid4"),
+      ];
+      //video js options loop for array access
+    for (let i = 0; i < myPlayer.length; i++)
+    //make all videos responsive to resolution
+      myPlayer[i].aspectRatio("16:9"),
+        myPlayer[i].fluid(!0),
+    //default audio
         myPlayer[i].volume(0.5);
-        
-        }
-
-    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
-        if (target.hasAttribute('data-target')) {
-            var m_ID = target.getAttribute('data-target');
-            document.getElementById(m_ID).classList.add('open');
-            e.preventDefault();
-
-            
-        }
+    //Open the modal when clicked on card
+    if (
+      target.hasAttribute("data-toggle") &&
+      "modal" == target.getAttribute("data-toggle") &&
+      target.hasAttribute("data-target")
+    ) {
+      var m_ID = target.getAttribute("data-target");
+      document.getElementById(m_ID).classList.add("open"), e.preventDefault();
     }
-
     // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
-    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
-        var modal = document.querySelector('[class="modal open"]');
-        modal.classList.remove('open');
+    if (
+      (target.hasAttribute("data-dismiss") &&
+        "modal" == target.getAttribute("data-dismiss")) ||
+      target.classList.contains("modal")
+    ) {
+      var modal;
+      document.querySelector('[class="modal open"]').classList.remove("open"),
         e.preventDefault();
-
-        //make video pause upon modal close
-        for (let i = 0; i < myPlayer.length; i++){
-        myPlayer[i].pause();               
-        }
-
+    //make video pause upon modal close
+      for (let i = 0; i < myPlayer.length; i++) {myPlayer[i].pause();}
     }
-
-}, false);
-
+  },
+  !1
+);
 const menu = document.getElementById("topbar");
-
-function fansyFunction(){
-
-document.getElementById("menu").classList.toggle("show")
+function fansyFunction() {
+  document.getElementById("menu").classList.toggle("show");
 }
--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,45 +1,24 @@
 var loader = document.getElementById("preloader");
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-
-
-
-//Preloader
-
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-
-    //Remove the line under the comment when using the intro animation.
-
-    // intro.style.top = '-100vh'
-    
-    //The intro animation, currently under development
-
-    
-
-    setTimeout(()=>{
-
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
-
-        setTimeout(()=>{
-            logoSpan.forEach((span, idx)=>{
-
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })
-        },2000);
-
-        setTimeout(()=>{
-            intro.style.top = '-100vh'
-        }, 2300)
-
-    })
-
-})
+let intro = document.querySelector(".intro"),
+  logo = document.querySelector(".logo-header"),
+  logoSpan = document.querySelectorAll(".logo");
+window.addEventListener("load", function () {
+  (loader.style.display = "none"),
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add("active");
+        }, 400 * (idx + 1));
+      }),
+        setTimeout(() => {
+          logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+              span.classList.remove("active"), span.classList.add("fade");
+            }, 50 * (idx + 1));
+          });
+        }, 2e3),
+        setTimeout(() => {
+          intro.style.top = "-100vh";
+        }, 2300);
+    });
+});
